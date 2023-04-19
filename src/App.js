40c78f959931815c56fa6bf3f3cb571fedd12128
LaurenTaylor92 <118805680+LaurenTaylor92@users.noreply.github.com>
2023-04-19 11:44:12 +1000

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lauren Taylor and is{" "}
+        <a
+          href="https://github.com/LaurenTaylor92/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
