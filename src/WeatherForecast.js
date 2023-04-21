4d867ae0e6604cc71cc0003b51aa73ca836cbef7
LaurenTaylor92 <118805680+LaurenTaylor92@users.noreply.github.com>
2023-04-22 09:11:59 +1000

    Updated API url to correct errors

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "1714417e6c721ab4697220c1050cd086";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
